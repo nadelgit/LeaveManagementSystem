c4793716857a26ef19a640e1549172d27da2748c
Nada Adel <nada_adel_mostafa@hotmail.com>
2025-08-24 20:26:57 +0300

    Adding the Service Layer with GetAll, Get with ID and Delete Methods

---
+++ LeaveManagementSystem.Web/Services/LeaveTypesService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using LeaveManagementSystem.Web.Data;
+using LeaveManagementSystem.Web.Models.LeaveTypes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagementSystem.Web.Services;
+
+public class LeaveTypesService(ApplicationDbContext context, IMapper mapper)
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public async Task<List<LeaveTypeReadOnlyVM>> GetAll()
+    {
+        var data = await _context.LeaveTypes.ToListAsync();   
+
+        //Using AutoMapper
+        var viewdata = _mapper.Map<List<LeaveTypeReadOnlyVM>>(data);
+        return (viewdata);
+    }
+
+    public async Task<T> Get<T>(int id) where T : class
+    {
+        var leaveType = await _context.LeaveTypes.FirstOrDefaultAsync(m => m.Id == id);
+
+        if (leaveType == null)
+        {
+            return null;
+        }
+        var viewdata = _mapper.Map<T>(leaveType);
+
+        return (viewdata);
+    }
+
+    public async Task Delete(int? id)
+    {
+        var data = await _context.LeaveTypes.FirstOrDefaultAsync(m => m.Id == id);
+        if(data!=null)
+        {
+            _context.LeaveTypes.Remove(data);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+
+}
